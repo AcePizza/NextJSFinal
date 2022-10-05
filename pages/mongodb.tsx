04f15f2e630b74b323b388b5fa67faa56ed56c4f
AcePizza <stianchrbakke@gmail.com>
2022-10-05 15:23:04 +0200

    cleaned some old code

--- pages/mongodb.tsx
+++ pages/mongodb.tsx
@@ -5,54 +5,6 @@ import { Props, Data } from "../@types";
 import React from "react";
 import MongoCard from "../components/MongoCard";
 
-// export async function getServerSideProps(context: any) {
-//   try {
-//     await clientPromise;
-//     // `await clientPromise` will use the default database passed in the MONGODB_URI
-//     // However you can use another database (e.g. myDatabase) by replacing the `await clientPromise` with the following code:
-//     //
-//     // `const client = await clientPromise`
-//     // `const db = client.db("myDatabase")`
-//     //
-//     // Then you can execute queries against your database like so:
-//     // db.find({}) or any of the MongoDB Node Driver commands
-
-//     return {
-//       props: { isConnected: true },
-//     };
-//   } catch (e) {
-//     console.error(e);
-//     return {
-//       props: { isConnected: false },
-//     };
-//   }
-// }
-
-// export default function Home({
-//   isConnected,
-// }: InferGetServerSidePropsType<typeof getServerSideProps>) {
-//   return (
-//     <div className="container">
-//       <main>
-//         <h1 className="title">
-//           Welcome to <a href="https://nextjs.org">Next.js with MongoDB!</a>
-//         </h1>
-
-//         {isConnected ? (
-//           <h2 className="subtitle" color="green">
-//             You are connected to MongoDB
-//           </h2>
-//         ) : (
-//           <h2 className="subtitle">
-//             You are NOT connected to MongoDB. Check the <code>README.md</code>{" "}
-//             for instructions.
-//           </h2>
-//         )}
-//       </main>
-//     </div>
-//   );
-// }
-
 export const getServerSideProps = async (props: Props) => {
   const res = await fetch("http://localhost:3000/api/products");
   const data: Data = await res.json();
