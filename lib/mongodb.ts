5d4cba94ad11b02cb37292f6adbf868a73ad0ab8
AcePizza <stianchrbakke@gmail.com>
2022-10-11 15:48:06 +0200

    cleaning

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -4,6 +4,7 @@ if (!process.env.MONGODB_URI) {
     throw new Error('Invalid environment variable: "MONGODB_URI"')
 }
 
+
 const uri = process.env.MONGODB_URI
 const options = {}
 
@@ -15,21 +16,16 @@ if (!process.env.MONGODB_URI) {
 }
 
 if (process.env.NODE_ENV === 'development') {
-    // In development mode, use a global variable so that the value
-    // is preserved across module reloads caused by HMR (Hot Module Replacement).
+    // Runns global
     if (!global._mongoClientPromise) {
-        console.log("Global");
         client = new MongoClient(uri, options)
         global._mongoClientPromise = client.connect()
     }
     clientPromise = global._mongoClientPromise
 } else {
-    // In production mode, it's best to not use a global variable.
-    console.log("Not global");
+    // Not global
     client = new MongoClient(uri, options)
     clientPromise = client.connect()
 }
 
-// Export a module-scoped MongoClient promise. By doing this in a
-// separate module, the client can be shared across functions.
 export default clientPromise
