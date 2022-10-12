d5baa05033f4756b05c9f250f189bd46edc2838d
AcePizza <stianchrbakke@gmail.com>
2022-10-12 15:20:21 +0200

    added resover for mutation

--- pages/api/graphql.tsx
+++ pages/api/graphql.tsx
@@ -1,4 +1,4 @@
-import { gql, ApolloServer, Config } from "apollo-server-micro";
+import { gql, ApolloServer } from "apollo-server-micro";
 import { NextApiRequest, NextApiResponse } from "next";
 import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
 import clientPromise from "../../lib/mongodb";
@@ -24,6 +24,16 @@ const typeDefs = gql`
   type Query {
     getProducts: [Product]
   }
+
+  type Mutation {
+    insertTest(testString: String, testNumber: Int): Test!
+  }
+
+  type Test {
+    _id: ID
+    testString: String
+    testNumber: Int
+  }
 `;
 
 const resolvers = {
@@ -44,6 +54,35 @@ const resolvers = {
       }
     },
   },
+
+  Mutation: {
+    insertTest: async <X, Y extends Test>(parent: X, args: Y) => {
+      console.log("parent :>> ", parent);
+      console.log("args :>> ", args);
+      try {
+        const client = await clientPromise;
+        const db = client.db("NEXTjsStore");
+
+        const insertSomething = await db.collection("Test").insertOne({
+          testString: args.testString,
+          testNumber: args.testNumber,
+        });
+        console.log("insertSomething :>> ", insertSomething.insertedId);
+        return {
+          _id: insertSomething.insertedId,
+          testString: args.testString,
+          testNumber: args.testNumber,
+        };
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  },
+};
+
+type Test = {
+  testString: string;
+  testNumber: number;
 };
 
 const apolloServer = new ApolloServer({
