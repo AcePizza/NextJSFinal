514dce86f6b6fb732d0c39f0473b96e9ab9518d6
AcePizza <stianchrbakke@gmail.com>
2022-10-19 10:58:11 +0200

    added product details page

--- pages/graphql/[id].tsx
+++ pages/graphql/[id].tsx
@@ -1,15 +1,32 @@
+import { QueryDataOptions } from "@apollo/client";
+import { removeFragmentSpreadFromDocument } from "@apollo/client/utilities";
 import { gql } from "apollo-server-micro";
+import { resolveObjectURL } from "buffer";
+import { InferGetServerSidePropsType } from "next";
+import { useRouter } from "next/router";
 import React from "react";
+import { resourceLimits } from "worker_threads";
 import { Product } from "../../@types";
 import client from "../../utils/apollo-client";
 
-type Products = Product[];
+export const RouteParams = () => {
+  const route = useRouter();
+  return { params: route.query };
+};
 
-export const getStaticPaths = async <Type,>(params: Type) => {
-  const { data } = await client.query<Products>({
+type Params = {
+  query: {
+    id: string;
+  };
+};
+
+export const getServerSideProps = async (params: Params) => {
+  const proID = Number(params.query.id);
+  console.log("proID :>> ", typeof proID);
+  const { data } = await client.query({
     query: gql`
       query Products {
-        getProducts {
+        getProduct(id: ${proID}) {
           _id
           id
           title
@@ -25,22 +42,83 @@ export const getStaticPaths = async <Type,>(params: Type) => {
       }
     `,
   });
-  const paths = data.map((product: Product) => {
-    return {
-      params: { id: product.id.toString() },
-    };
-  });
-  return { paths, fallback: false };
+  const propsData = {
+    id: data.getProduct.id,
+    title: data.getProduct.title,
+    price: data.getProduct.price,
+    description: data.getProduct.description,
+    category: data.getProduct.category,
+    image: data.getProduct.image,
+    rating: {
+      rate: data.getProduct.rating.rate,
+      count: data.getProduct.rating.count,
+    },
+  };
+  console.log("propsData :>> ", propsData);
+  return {
+    props: propsData,
+  };
 };
 
-export const getStaticProps = async <Type,>(context: Type) => {
-  console.log("context", context);
-};
-
-const Details = <Type,>(props: Type) => {
+const Details = (props: Product) => {
+  console.log("props :>> ", props);
   return (
-    <div>
+    <div className="container">
       <h3>Here is the details page</h3>
+      <div className="col-lg-8 border p-3 main-section bg-white">
+        <div className="row m-0">
+          <div className="col-lg-4 left-side-product-box pb-3">
+            <img
+              src={props.image}
+              height={"auto"}
+              width={"330px"}
+              className="border p-3"
+            />
+          </div>
+          <div className="col-lg-8">
+            <div className="right-side-pro-detail border p-3 m-0">
+              <div className="row">
+                <div className="col-lg-12">
+                  <span>{props.title}</span>
+                  <p className="m-0 p-0">{props.category}</p>
+                </div>
+                <div className="col-lg-12">
+                  <p className="m-0 p-0 price-pro">${props.price}</p>
+                  <hr className="p-0 m-0" />
+                </div>
+                <div className="col-lg-12 pt-2">
+                  <h5>Product Detail</h5>
+                  <span>{props.description}</span>
+                  <hr className="m-0 pt-2 mt-2" />
+                </div>
+
+                <div className="col-lg-12">
+                  <h6>Quantity :</h6>
+                  <input
+                    type="number"
+                    className="form-control text-center w-100"
+                    defaultValue="1"
+                  />
+                </div>
+                <div className="col-lg-12 mt-3">
+                  <div className="row">
+                    <div className="col-lg-6 pb-2">
+                      <a href="#" className="btn btn-danger w-100">
+                        Add To Cart
+                      </a>
+                    </div>
+                    <div className="col-lg-6">
+                      <a href="#" className="btn btn-success w-100">
+                        Shop Now
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
